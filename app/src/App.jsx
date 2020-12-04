f2d31a0adc8652a870e1d9c1127cd96988af6d4b
Drew Ross <mail.drew.ross@gmail.com>
2020-12-04 15:28:17 -0800

    Render Sidebar component.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
+    <div className='App'>
+      <Sidebar />
     </div>
   );
 };
