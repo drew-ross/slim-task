edfdfa36fd76d58abd09e7db6f9fcb95e339f82b
Drew Ross <mail.drew.ross@gmail.com>
2020-12-08 14:33:54 -0800

    Add custom svg icons for Sidebar.

---
+++ app/src/svg/index.js
@@ -0,0 +1,17 @@
+import check from './check.svg';
+import checkSolid from './checkSolid.svg';
+import close from './close.svg';
+import hamburger from './hamburger.svg';
+import timer from './timer.svg';
+import timerSolid from './timerSolid.svg';
+
+const icons = {
+  check,
+  checkSolid,
+  close,
+  hamburger,
+  timer,
+  timerSolid
+}
+
+export default icons;
