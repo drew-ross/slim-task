54349b4b0571700bbd15619b257f5ee3df73da35
Drew Ross <mail.drew.ross@gmail.com>
2020-12-08 10:22:24 -0800

    Render WidgetContainer with child TaskList widget.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import WidgetContainer, { TaskList } from './components/Widgets';
 
 const App = () => {
   return (
     <div className='App'>
       <Sidebar />
+      <WidgetContainer component={TaskList} isOn={true}/>
     </div>
   );
 };
