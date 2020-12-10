4bee913e1c04ad804e77fff62055ed74abea6fe5
Drew Ross <mail.drew.ross@gmail.com>
2020-12-10 14:36:08 -0800

    Add ButtonHoverMessage component.

---
+++ app/src/components/MenuButton/ButtonHoverMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ButtonHoverMessage = ({ message }) => {
+
+  return (
+    <div className='ButtonHoverMessage'>
+      <p>{message}</p>
+    </div>
+  );
+};
