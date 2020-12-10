dd305e2c7262e0d4bd62fb37cc64ed2cd7bca6e0
Drew Ross <mail.drew.ross@gmail.com>
2020-12-10 14:35:39 -0800

    Render ButtonHoverMessage on hover. Add surrounding div element.

--- app/src/components/MenuButton/MenuButton.jsx
+++ app/src/components/MenuButton/MenuButton.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ButtonHoverMessage } from './ButtonHoverMessage';
 
 export const MenuButton = props => {
-
   const { isMain, widgetName, toggleWidget, isOn, setSidebarVisible } = props;
   const { icon, ariaLabel } = props.menuButton;
+  const [hoverMessageVisible, setHoverMessageVisible] = useState(false);
 
   const handleClick = e => {
     if (widgetName && toggleWidget) {
@@ -18,16 +19,22 @@ export const MenuButton = props => {
   }, [isOn]);
 
   return (
-    <button
-      className={isMain ? 'MenuButton MenuButton--main' : 'MenuButton'}
-      aria-label={isOn ? ariaLabel.on : ariaLabel.off}
-      onClick={handleClick}
-    >
-      {isOn ?
-        <img src={icon.on} alt={ariaLabel.on + ' icon'} />
-        :
-        <img src={icon.off} alt={ariaLabel.off + ' icon'} />
+    <div className={isMain ? 'MenuButton MenuButton--main' : 'MenuButton'}>
+      <button
+        aria-label={isOn ? ariaLabel.on : ariaLabel.off}
+        onClick={handleClick}
+        onMouseEnter={() => setHoverMessageVisible(true)}
+        onMouseLeave={() => setHoverMessageVisible(false)}
+      >
+        {isOn ?
+          <img src={icon.on} alt={ariaLabel.on + ' icon'} />
+          :
+          <img src={icon.off} alt={ariaLabel.off + ' icon'} />
+        }
+      </button>
+      {hoverMessageVisible &&
+        (<ButtonHoverMessage message={isOn ? ariaLabel.on : ariaLabel.off} />)
       }
-    </button>
+    </div>
   );
 };
