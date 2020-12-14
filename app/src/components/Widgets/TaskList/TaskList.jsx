ddcc109d6a22ee1ddac03349f7e6bbd58a6d1038
Drew Ross <mail.drew.ross@gmail.com>
2020-12-14 09:49:01 -0800

    Move TaskList to subfolder.

---
+++ app/src/components/Widgets/TaskList/TaskList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const TaskList = props => {
+
+  return (
+    <section className='Widget TaskList'>
+      <h2>Tasks</h2>
+      <p>Test Widget</p>
+    </section>
+  )
+}
