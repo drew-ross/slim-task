826a29c456c79824d099621bdd84be8d4fb0a45b
Drew Ross <mail.drew.ross@gmail.com>
2020-12-14 11:52:59 -0800

    Add dummy task list and render.

--- app/src/components/Widgets/TaskList/TaskList.jsx
+++ app/src/components/Widgets/TaskList/TaskList.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Task } from './Task';
+
+const dummyTasks = [
+  {
+    id: 1,
+    taskName: 'Dummy Task',
+    complete: true,
+  },
+  {
+    id: 2,
+    taskName: 'Another Dummy Task',
+    complete: false,
+  }
+];
 
 export const TaskList = props => {
 
   return (
     <section className='Widget TaskList'>
       <h2>Tasks</h2>
-      <p>Test Widget</p>
+      {dummyTasks.length &&
+        dummyTasks.map(task => (
+          <Task
+            key={task.id}
+            id={task.id}
+            taskName={task.taskName}
+            complete={task.complete}
+          />
+        ))
+      }
     </section>
-  )
-}
+  );
+};
