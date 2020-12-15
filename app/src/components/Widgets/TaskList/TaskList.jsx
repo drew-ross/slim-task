2b710f4d8de36e37874da3b1949d6e19531b8d25
Drew Ross <mail.drew.ross@gmail.com>
2020-12-14 16:33:22 -0800

    Add input for creating new tasks.

--- app/src/components/Widgets/TaskList/TaskList.jsx
+++ app/src/components/Widgets/TaskList/TaskList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { addTask, updateTask, removeTask } from '../../../state/actions/taskListActions';
@@ -6,11 +6,38 @@ import { addTask, updateTask, removeTask } from '../../../state/actions/taskList
 import { Task } from './Task';
 
 const TaskList = props => {
-  const { taskList, updateTask, removeTask } = props;
+  const { taskList, updateTask, removeTask, addTask } = props;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChanges = e => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addTask(inputValue);
+    setInputValue('');
+  };
 
   return (
     <section className='Widget TaskList'>
       <h2>Tasks</h2>
+      <form onSubmit={handleSubmit}>
+        <label
+          htmlFor='input-new-task'
+          className='label--hidden'
+        >
+          New Task
+        </label>
+        <input
+          id='input-new-task'
+          placeholder='New Task'
+          value={inputValue}
+          onChange={handleInputChanges}
+        />
+        <button>Add</button>
+      </form>
       {taskList &&
         taskList.map(task => (
           <Task
