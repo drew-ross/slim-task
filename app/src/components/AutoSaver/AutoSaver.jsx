6885a20e23417fba7999c7631f9f3e88cd9d39be
Drew Ross <mail.drew.ross@gmail.com>
2020-12-29 14:13:57 -0800

    Connect to store. Add functionality to save state changes automatically (localStorage for now).

--- app/src/components/AutoSaver/AutoSaver.jsx
+++ app/src/components/AutoSaver/AutoSaver.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
-const AutoSaver = () => {
-  return (
-    <></>
-  );
+const AutoSaver = props => {
+  const taskList = props.taskList;
+  const [firstLoad, setFirstLoad] = useState(true);
+
+  useEffect(() => {
+    console.log(taskList);
+    if (firstLoad) {
+      setFirstLoad(false);
+    } else {
+      saveChanges("taskList", taskList);
+    }
+  });
+
+  const saveChanges = (name, state) => {
+    localStorage.setItem(name, JSON.stringify(state));
+  };
+
+  return null;
 };
 
+const mapStateToProps = state => {
+  return {
+    taskList: state.taskListReducer
+  };
+};
 
-
-const ConnectedAutoSaver = connect(null, {})(AutoSaver);
+const ConnectedAutoSaver = connect(mapStateToProps, {})(AutoSaver);
 
 export { ConnectedAutoSaver };
