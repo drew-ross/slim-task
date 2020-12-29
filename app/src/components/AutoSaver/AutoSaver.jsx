73fcfe428826d52a5e5d8395d579e38358d12cef
Drew Ross <mail.drew.ross@gmail.com>
2020-12-29 11:23:26 -0800

    Create AutoSaver component.

---
+++ app/src/components/AutoSaver/AutoSaver.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const AutoSaver = () => {
+  return (
+    <></>
+  );
+};
+
+
+
+const ConnectedAutoSaver = connect(null, {})(AutoSaver);
+
+export { ConnectedAutoSaver };
