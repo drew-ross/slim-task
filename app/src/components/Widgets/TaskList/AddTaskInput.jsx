0a08ac9898f78af8b3340e9fdeed12f05f0b3306
Drew Ross <mail.drew.ross@gmail.com>
2021-01-04 13:43:42 -0800

    Add logic to show/hide form with button/submit.

--- app/src/components/Widgets/TaskList/AddTaskInput.jsx
+++ app/src/components/Widgets/TaskList/AddTaskInput.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 export const AddTaskInput = props => {
   const { addTask } = props;
+  const [showForm, setShowForm] = useState(false);
   const [inputValue, setInputValue] = useState('');
 
   const handleInputChanges = e => {
@@ -13,23 +14,41 @@ export const AddTaskInput = props => {
     e.preventDefault();
     addTask(inputValue);
     setInputValue('');
+    setShowForm(false);
+  };
+
+  const handleShowForm = () => {
+    setShowForm(true);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label
-        htmlFor='input-new-task'
-        className='label--hidden'
-      >
-        New Task
+    <div className='AddTaskInput'>
+      { showForm ?
+        (<form
+          className='form-single-input'
+          onSubmit={handleSubmit}>
+          <label
+            htmlFor='input-new-task'
+            className='label--hidden'
+          >
+            New Task
         </label>
-      <input
-        id='input-new-task'
-        placeholder='New Task'
-        value={inputValue}
-        onChange={handleInputChanges}
-      />
-      <button>Add</button>
-    </form>
+          <input
+            id='input-new-task'
+            placeholder='New Task'
+            value={inputValue}
+            onChange={handleInputChanges}
+          ></input>
+          <button>Add</button>
+        </form>)
+        :
+        (<button
+          className='btn-show-form'
+          onClick={handleShowForm}
+        >
+          +
+        </button>)}
+
+    </div>
   );
 };
