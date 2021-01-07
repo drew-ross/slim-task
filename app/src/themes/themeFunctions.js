9bb038051c8b8064287415e9dddb830c7d608749
Drew Ross <mail.drew.ross@gmail.com>
2021-01-07 10:11:50 -0800

    Remove console log.

--- app/src/themes/themeFunctions.js
+++ app/src/themes/themeFunctions.js
@@ -2,6 +2,5 @@ import { themes } from './themeDefinitions';
 
 // Theme: name of theme in themes object  |  Selector: background/font
 export const getThemeStyle = (theme, selector) => {
-  console.log({ [selector]: themes[theme][selector] });
   return { [selector]: themes[theme][selector] };
 };
