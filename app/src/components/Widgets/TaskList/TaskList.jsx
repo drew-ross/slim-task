5952d7d614baf95e921e3d6abe0ec4415879a1ed
Drew Ross <mail.drew.ross@gmail.com>
2021-01-07 10:19:05 -0800

    Remove unused import.

--- app/src/components/Widgets/TaskList/TaskList.jsx
+++ app/src/components/Widgets/TaskList/TaskList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import { addTask, updateTask, removeTask, toggleShowCompleted } from '../../../state/actions/taskListActions';
