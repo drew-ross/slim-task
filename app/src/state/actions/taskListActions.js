0715033cf07383ae0b9e804d561170a12df25e47
Drew Ross <mail.drew.ross@gmail.com>
2021-01-07 12:45:41 -0800

    Add setTaskListTheme action.

--- app/src/state/actions/taskListActions.js
+++ app/src/state/actions/taskListActions.js
@@ -3,6 +3,7 @@ export const UPDATE_TASK = 'UPDATE_TASK';
 export const REMOVE_TASK = 'REMOVE_TASK';
 export const SET_TASKLIST_STATE = 'SET_TASKLIST_STATE';
 export const TOGGLE_SHOW_COMPLETED = 'TOGGLE_SHOW_COMPLETED';
+export const SET_TASKLIST_THEME = 'SET_TASKLIST_THEME';
 
 export const setTaskListState = (taskListState) => {
   return { type: SET_TASKLIST_STATE, payload: taskListState };
@@ -22,4 +23,9 @@ export const removeTask = (uuid) => {
 
 export const toggleShowCompleted = () => {
   return { type: TOGGLE_SHOW_COMPLETED };
+};
+
+export const setTaskListTheme = (theme) => {
+  console.log(theme);
+  return { type: SET_TASKLIST_THEME, payload: theme };
 };
